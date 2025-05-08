b2aa898ffa1262ff28575c359f44c5c15faca144
andreimdrs <andreimdrs19@gmail.com>
2025-05-08 16:54:11 -0300

    primeira aula

---
+++ andreium/src/Compodente.jsx
@@ -0,0 +1,11 @@
+function Compodente () {
+    const nome = "andrei lee"
+    return (
+       <div>
+            <h1>Hello World!</h1>
+            <p>Seu nome é {nome}.</p>
+       </div>
+    )
+}
+
+export default Compodente
